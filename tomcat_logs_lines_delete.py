fef9e4cf15d345204df5b4a187409d606b63e34d
Nikita <nik.matue@gmail.com>
2016-08-18 15:51:19 +0500

    улучшено отображение вывода

--- tomcat_logs_lines_delete.py
+++ tomcat_logs_lines_delete.py
@@ -1,14 +1,14 @@
 from os import listdir
 files = listdir(".") # листинг текущей директории
 LogFilesNames = filter(lambda x: x.endswith('.log'), files) # отобрать все лог файлы
-print('Files:',len(LogFilesNames))
+print'Files:',len(LogFilesNames)
 lc=0
 for n in LogFilesNames:    # цикл по именам файлов
     f = open(n,'r+').readlines() # читаем строки каждого файла
-    print('File: ',n,'Lines: ',len(f)) 
+    print'File: ',n,'\nLines: ',len(f)
     for i in range(len(f)): # цикл по номерам строк
         if 'pte_deleted' in f[i]:
-            pass
+            lc+=0 # счетчик 
         elif 'c.s.s.c.jdbc.query.ScriptQuery - pan'  in f[i]:# условие сравнения 
             f[i]='-- c.s.s.c.jdbc.query.ScriptQuery - pan: pte_deleted@serebrennikovn --\n' # правка строк в файле
             lc+=1
@@ -17,12 +17,5 @@ for n in LogFilesNames:    # цикл по именам файлов
             lc+=1
     with open(n,'w') as F:
         F.writelines(f) # применение изменений в файле
-        print('Edited lines: ',lc) # вывод числа отредактированных строк
-        print('Applying in file: ',F.name) # вывод количества обработанных файлов
-        
-
-
-
-
-
-
+        print'Edited lines: ',lc # вывод числа отредактированных строк
+        print'Applying in file: ',F.name # вывод имени обработанного файла
