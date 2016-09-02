3e94e7a1525457e3681b40371d66bd0070bf023e
Nikita <nik.matue@gmail.com>
2016-09-02 14:40:54 +0500

    Update mask pans.py
    добавлена функция GetLogs, улучшена читабельность кода

--- mask pans.py
+++ mask pans.py
@@ -7,23 +7,23 @@
 
 '''
 
-import os, re
+import os
+import re
+def GetLogs(x):
+    m=[]
+    for i in x:
+        if i.endswith('.log'):
+            m.append(i)
+    return m
 files = os.listdir(".") # листинг текущей директории
-LogFilesNames = filter(lambda x: x.endswith('.log'), files) # отобрать все лог файлы
+LogFilesNames=GetLogs(files) # отобрать все лог файлы
 print'Files:',len(LogFilesNames)
-Pattern=r'\D(\d{4})\d{8}(\d{4})\D' #паттерн 
-Mask=r' \1 **** **** \2 ' #маска
+Pattern=r'\D(\d{4})\d{8}(\d{4})\D' 
+Mask=r' \1 **** **** \2 ' 
 for n in LogFilesNames:    # цикл по именам файлов
-    f = open(n,'r').readlines() # читаем строки файла
-    print'File: ',n,'\nLines: ',len(f)
-    f_text=''.join(f)  # инициируем переменную f_text и помещаем в нее текстовое содержимое файла
+    f = open(n,'r') 
+    f_text=f.read()  # инициируем переменную f_text и помещаем в нее текстовое содержимое файла
     f_new_text = re.sub(Pattern, Mask, f_text) # в f_new_text пишем обработанный с помощью регулярного выражения текст
-    f = open(n, 'w') #открытие файла для записи
-    print'Applying in file: ',n # вывод имени обработанного файла
-    f.write(f_new_text) #запись в файл преобразованного текста
-    f.close() #закрытие файла
-
-    
-
-
-
+    with open(n, 'w') as f: 
+        print'Applying in file: ',n # вывод имени обработанного файла
+        f.write(f_new_text) 
