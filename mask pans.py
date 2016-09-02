79ed992aba4eaa69f594a9837cba27f48e4e0373
Nikita <nik.matue@gmail.com>
2016-09-02 14:45:48 +0500

    Update mask pans.py

--- mask pans.py
+++ mask pans.py
@@ -9,18 +9,18 @@
 
 import os
 import re
-def GetLogs(x):
+def get_logs(x):
     m=[]
     for i in x:
         if i.endswith('.log'):
             m.append(i)
     return m
 files = os.listdir(".") # листинг текущей директории
-LogFilesNames=GetLogs(files) # отобрать все лог файлы
-print'Files:',len(LogFilesNames)
-Pattern=r'\D(\d{4})\d{8}(\d{4})\D' 
-Mask=r' \1 **** **** \2 ' 
-for n in LogFilesNames:    # цикл по именам файлов
+log_files_names=get_logs(files) # отобрать все лог файлы
+print'Files:',len(log_files_names)
+pattern=r'\D(\d{4})\d{8}(\d{4})\D' 
+mask=r' \1 **** **** \2 ' 
+for n in log_files_names:    # цикл по именам файлов
     f = open(n,'r') 
     f_text=f.read()  # инициируем переменную f_text и помещаем в нее текстовое содержимое файла
     f_new_text = re.sub(Pattern, Mask, f_text) # в f_new_text пишем обработанный с помощью регулярного выражения текст
